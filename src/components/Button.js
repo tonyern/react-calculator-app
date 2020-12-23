da6527753e587795e76dd6d7dfadfe61623e1ca3
Tony <tony.d.nguyen@protonmail.com>
2020-12-23 16:14:00 -0600

    Added console logs to all button clicks.

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,37 +4,64 @@ import './Button.css';
 class Button extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            input: ''
+        }
+    }
+
+    numberPressedHandler = (key, value) => {
+        console.log(value + " was pressed");
+        this.setState({
+            [key]: value
+        })
     }
 
     render() {
         return (
             <div>
                 <div>
-                    <button type="button" className="calculator-btn"><h1>7</h1></button>
-                    <button type="button" className="calculator-btn"><h1>8</h1></button>
-                    <button type="button" className="calculator-btn"><h1>9</h1></button>
-                    <button type="button" className="calculator-btn operator"><h1>/</h1></button>
+                    <button type="button" className="calculator-btn"
+                        onClick={event => this.numberPressedHandler("input", "7")}><h1>7</h1></button>
+                    <button type="button" className="calculator-btn"
+                        onClick={event => this.numberPressedHandler("input", "8")}><h1>8</h1></button>
+                    <button type="button" className="calculator-btn"
+                        onClick={event => this.numberPressedHandler("input", "9")}><h1>9</h1></button>
+                    <button type="button" className="calculator-btn operator"
+                        onClick={event => this.numberPressedHandler("input", "/")}><h1>÷</h1></button>
                 </div>
 
                 <div>
-                    <button type="button" className="calculator-btn"><h1>4</h1></button>
-                    <button type="button" className="calculator-btn"><h1>5</h1></button>
-                    <button type="button" className="calculator-btn"><h1>6</h1></button>
-                    <button type="button" className="calculator-btn operator"><h1>X</h1></button>
+                    <button type="button" className="calculator-btn"
+                        onClick={event => this.numberPressedHandler("input", "4")}><h1>4</h1></button>
+                    <button type="button" className="calculator-btn"
+                        onClick={event => this.numberPressedHandler("input", "5")}><h1>5</h1></button>
+                    <button type="button" className="calculator-btn"
+                        onClick={event => this.numberPressedHandler("input", "6")}><h1>6</h1></button>
+                    <button type="button" className="calculator-btn operator"
+                        onClick={event => this.numberPressedHandler("input", "*")}><h1>×</h1></button>
                 </div>
 
                 <div>
-                    <button type="button" className="calculator-btn"><h1>1</h1></button>
-                    <button type="button" className="calculator-btn"><h1>2</h1></button>
-                    <button type="button" className="calculator-btn"><h1>3</h1></button>
-                    <button type="button" className="calculator-btn operator"><h1>+</h1></button>
+                    <button type="button" className="calculator-btn"
+                        onClick={event => this.numberPressedHandler("input", "1")}><h1>1</h1></button>
+                    <button type="button" className="calculator-btn"
+                        onClick={event => this.numberPressedHandler("input", "2")}><h1>2</h1></button>
+                    <button type="button" className="calculator-btn"
+                        onClick={event => this.numberPressedHandler("input", "3")}><h1>3</h1></button>
+                    <button type="button" className="calculator-btn operator"
+                        onClick={event => this.numberPressedHandler("input", "+")}><h1>+</h1></button>
                 </div>
 
                 <div>
-                    <button type="button" className="calculator-btn"><h1>.</h1></button>
-                    <button type="button" className="calculator-btn"><h1>0</h1></button>
-                    <button type="button" className="calculator-btn"><h1>=</h1></button>
-                    <button type="button" className="calculator-btn operator"><h1>-</h1></button>
+                    <button type="button" className="calculator-btn"
+                        onClick={event => this.numberPressedHandler("input", ".")}><h1>.</h1></button>
+                    <button type="button" className="calculator-btn"
+                        onClick={event => this.numberPressedHandler("input", "0")}><h1>0</h1></button>
+                    <button type="button" className="calculator-btn"
+                        onClick={event => this.numberPressedHandler("input", "=")}><h1>=</h1></button>
+                    <button type="button" className="calculator-btn operator"
+                        onClick={event => this.numberPressedHandler("input", "-")}><h1>-</h1></button>
                 </div>
             </div>
         );
--- src/components/Clear.js
+++ src/components/Clear.js
@@ -6,10 +6,14 @@ class Clear extends Component {
         super(props);
     }
 
+    clearInput = () => {
+        console.log('Clearing input');
+    }
+
     render() {
         return (
             <div>
-                <button type="button" id="clear-btn"><h1>CLEAR</h1></button>
+                <button type="button" id="clear-btn" onClick={this.clearInput}><h1>CLEAR</h1></button>
             </div>
         );
     }
