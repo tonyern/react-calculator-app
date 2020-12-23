b41fc69b18c93ddb3c75bd85b43596ddd9719eda
Tony <tony.d.nguyen@protonmail.com>
2020-12-23 15:52:14 -0600

    Implemented refs on input.

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,12 +4,24 @@ import './Input.css';
 class Input extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            input: ''
+        }
+
+        this.inputRef = React.createRef();
+    }
+
+    componentDidMount = () => {
+        if (this.inputRef) {
+            this.inputRef.current.focus();
+        }
     }
 
     render() {
         return (
             <div>
-                <input type="text" name="number-input" id="input-box" />
+                <input type="text" name="number-input" id="input-box" ref={this.inputRef} />
             </div>
         );
     }
